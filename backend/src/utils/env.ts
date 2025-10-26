f8ecd0bae0406794f42fa7f734427c69e29dfe16
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2025-10-26 08:19:54 +0300

    minor fix

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 export const ENV = {
   PORT: Number(process.env.PORT) || 5000,
   JWT_SECRET: process.env.JWT_SECRET || "default_secret",
-  DATABASE_URL: process.env.DATABASE_URL!,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://project_tracker_db_5esk_user:axUSda38Tp4ELuOWgSY7tAXamzBbSeQY@dpg-d3uivtbe5dus739ntme0-a.oregon-postgres.render.com/project_tracker_db_5esk",
   CORS_ORIGIN: process.env.CORS_ORIGIN || "*",
 };
